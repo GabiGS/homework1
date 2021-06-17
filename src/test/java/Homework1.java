928b9fa4d0d4f56afc1d849545d905b75c3aadfa
Gabi <ggonzalez@npsstore.net>
2021-06-17 10:50:48 -0600

    adding code

---
+++ src/test/java/Homework1.java
@@ -0,0 +1,24 @@
+import maintest.Base;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Homework1 extends Base {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod(){
+        driver = initializeDriver();
+    }
+
+    @Test
+    public void testHelloWorld(){
+        driver.get("https://rpp.pe/");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
+}
